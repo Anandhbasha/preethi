b87bdd957ac662d58e85408c2ddc6d132f16c6f9
Anandhbasha <anadhakm@gmail.com>
2025-07-08 12:15:54 +0530

    basics

---
+++ index.py
@@ -0,0 +1,38 @@
+# print("This is Basics of Python", end='---')
+# print("About Print Statement")
+# print("Hello","welcome")
+# print("Hello","welcome" ,sep='/')
+
+# varibale
+# int
+# float
+# boolean
+# string
+a=10
+print(type(a))
+name = "xyz"
+print(type(name))
+isworking = True
+
+# complex data types
+
+# list
+arr = [10,50.5,"abc",True]
+# tuple
+# set
+# Dic
+
+# operators
+# arithmetic
+# +,-,*,/,%,**
+a=20
+b=10
+print("addition Value is",a+b)
+print(f'The Sub value is {a-b}')
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+value = int(input("Enter the First value"))
+value2 = int(input("Enter Second value"))
+print(f'The addition value of value1 & value2 is {value+value2}')
